1f47dee7b024a925df1e6e0cc801e9580d485c54
bowyer-app <bowyer.app@gmail.com>
2022-10-09 17:38:06 +0900

    refactor menu_button

--- lib/ui/component/menu/menu_button.dart
+++ lib/ui/component/menu/menu_button.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class MenuButton extends StatelessWidget {
+class MenuButton extends HookConsumerWidget {
   const MenuButton({
     Key? key,
     required this.onTap,
@@ -16,29 +16,25 @@ class MenuButton extends StatelessWidget {
   final String? menuText;
 
   @override
-  Widget build(BuildContext context) {
-    return HookConsumer(
-      builder: (BuildContext context, ref, child) {
-        final appColors = ref.watch(appThemeProvider).appColors;
-        final l10n = L10n.of(context)!;
-        final menu = Text(
-          menuText ?? l10n.save,
-          style: TextStyle(
-            color: appColors.primaryTextColorAccent,
-            fontSize: 16,
-            fontWeight: FontWeight.w600,
-          ),
-        );
-        return AppButtonWidget(
-          child: Container(
-            margin: const EdgeInsets.only(
-              right: 16,
-            ),
-            child: menu,
-          ),
-          onTap: onTap,
-        );
-      },
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appColors = ref.watch(appThemeProvider).appColors;
+    final l10n = L10n.of(context)!;
+    final menu = Text(
+      menuText ?? l10n.save,
+      style: TextStyle(
+        color: appColors.primaryTextColorAccent,
+        fontSize: 16,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+    return AppButtonWidget(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          right: 16,
+        ),
+        child: menu,
+      ),
     );
   }
 }
